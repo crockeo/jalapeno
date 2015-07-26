be5056059176beb2e5dbce06a70451fbcf4f3549
Cerek Hillen <crockeo@gmail.com>
2015-07-25 21:34:12 -0400

    Started to implement the ability to have IO integrated into the Behaviors.

--- src/FRP/Jalapeno/Behavior.hs
+++ src/FRP/Jalapeno/Behavior.hs
@@ -3,6 +3,7 @@ module FRP.Jalapeno.Behavior where
 
 -------------
 -- Imports --
+import Control.Monad.IO.Class
 import Control.Applicative
 import Data.Monoid
 
@@ -13,18 +14,40 @@ import Data.Monoid
 type Time = Double
 
 -- | A continuous behavior along a stream of time.
-data Behavior a = Behavior (Time -> a)
+data Behavior a = Behavior   (Time ->    a)
+                | BehaviorIO (Time -> IO a)
 
 instance Functor Behavior where
-  fmap fn (Behavior a) =
-    Behavior $ \t -> fn (a t)
+  fmap fn (Behavior   a) = Behavior   $ \t ->      fn (a t)
+  fmap fn (BehaviorIO a) = BehaviorIO $ \t -> fmap fn (a t)
 
 instance Applicative Behavior where
   pure a = Behavior $ \_ -> a
 
+  -- TODO: More applicative instances.
   (Behavior fn) <*> (Behavior a) =
     Behavior $ \t ->
       fn t (a t)
+
+instance Monad Behavior where
+  return = pure
+
+  -- Binding a Behavior that doesn't produce an IO value.
+  (Behavior a) >>= fn =
+    Behavior $ \t ->
+      let (Behavior b) = fn (a t) in
+        b t
+
+  -- Binding a Behavior that produces an IO value.
+  (BehaviorIO a) >>= fn =
+    BehaviorIO $ \t -> do
+      av <- a t
+      case fn av of
+        (Behavior   b) -> return $ b t
+        (BehaviorIO b) ->          b t
+
+instance MonadIO Behavior where
+  liftIO a = BehaviorIO $ \t -> a
 
 -- | The @'Num'@ instance is really just a bunch of the appropriate function
 --   applications to the @'Applicative'@ instance.
